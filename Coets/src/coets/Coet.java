53bfc31d3fe63cb0009565345b5fa4ac6431bc76
OEsquivias <uri_eskivias@hotmail.com>
2018-11-21 19:11:04 +0100

    modification fase2

--- Coets/src/coets/Coet.java
+++ Coets/src/coets/Coet.java
@@ -1,28 +1,63 @@
 package coets;
 
 public class Coet {
-	
-	private String codi;
-	private int[] propulsor;
-	
+
+	private String id;
+	private int[] pmax;
+	private int[] pnow;
+
 	public Coet() {}
 	
-	public Coet(String codi, int[] propulsor) {
-		this.codi = codi;
-		this.propulsor = propulsor;
+	public Coet(String id, int[] pmax) {
+
+		this.id = are8Chars(id);
+		this.pmax = pmax;
+		this.pnow = new int[pmax.length];
+
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = are8Chars(id);
+	}
+
+	public int[] getMaxPower() {
+		return pmax;
+	}
+
+	public void setMaxPower(int[] pmax) {
+		this.pmax = pmax;
+	}
+
+	public int[] getPnow() {
+		return pnow;
+	}
+
+	public int getSpeed() {
+		int speed = 0;
+		for (int s : pnow) {
+			speed += s;
+		}
+		return speed;
+	}
+
+	public static String are8Chars(String id) {
+		if (id.length() != 8) {
+			throw new IllegalArgumentException("The id must be 8 character long");
+		} else {
+			return id;
+		}
 	}
 	
-	public String getCodi() {
-		return codi;
-	}
-	public void setCodi(String codi) {
-		this.codi = codi;
-	}
-	public int[] getPropulsor() {
-		return propulsor;
-	}
-	public void setPropulsor(int[] propulsor) {
-		this.propulsor = propulsor;
+	
+	public void accelerar() {
+		for (int i = 0; i > pnow.length; i++) {
+			pnow[i] = pnow[i] + 10;
+		}
+
 	}
 
 }
---
+++ Coets/src/coets/Engine.java
@@ -0,0 +1,14 @@
+package coets;
+
+public class Engine {
+
+	public Engine() {}
+	
+//	public Engine(int pmax) {
+//		this.pmax = pmax;
+//		this.pnow = new int[pmax.length];
+//	}
+	
+	
+	
+}
